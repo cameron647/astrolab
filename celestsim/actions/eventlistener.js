735824c8d929232ff5376a31a92bb4596f948ebb
Cameron Bass <140853948+cameronbass647@users.noreply.github.com>
2023-12-27 14:06:58 -0500

    filepath changes.

--- celestsim/actions/eventlistener.js
+++ celestsim/actions/eventlistener.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'GLTFLoader';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 // ---------------------------ADD PLANET FUNCTIONS-------------------------------------
 export function addPlanet(modelUrl, scene, camera, renderer, planetsArray, addMarsEnabled, planetSettings, scale, planetProperties, cameraDirection) {
